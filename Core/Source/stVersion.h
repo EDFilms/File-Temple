50df1205023b3c316ac5aa52cb0b32f145c80eaa
Robin Southern <betajaen@ihoed.com>
2018-02-07 10:17:12 +0000

    stVersion fixed

--- Core/Source/stVersion.h
+++ Core/Source/stVersion.h
@@ -1,21 +1,9 @@
-// Copyright 2018 E*D Films. All Rights Reserved.
-
-/**
- * [[[FILE NAME]]]
- *
- * [[[BREIF DESCRIPTION]]]
- * 
- * @author  dotBunny <hello@dotbunny.com>
- * @version 1
- * @since	  1.0.0
- */
-
-//SceneTrack,0,3,321
+//SceneTrack,0,3,326
 #ifndef SCENETRACK_VERSION_H
 #define SCENETRACK_VERSION_H
 #define SCENETRACK_VERSION_NAME "SceneTrack"
 #define SCENETRACK_VERSION_MAJOR 0
 #define SCENETRACK_VERSION_MINOR 3
-#define SCENETRACK_VERSION_BUILD 321
-#define SCENETRACK_VERSION_STR "SceneTrack 0.3.321"
+#define SCENETRACK_VERSION_BUILD 326
+#define SCENETRACK_VERSION_STR "SceneTrack 0.3.326"
 #endif
